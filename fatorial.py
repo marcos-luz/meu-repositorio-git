050c128e793207006b5f8d813a8c828daa57c9ea
marcos-luz <59104567+marcos-luz@users.noreply.github.com>
2020-11-02 21:39:25 -0300

    Add files via upload

---
+++ fatorial.py
@@ -0,0 +1,8 @@
+print("-" * 50)
+print((" " * 14) + "Calculador de Fatorial")
+print("-" * 50)
+print("\n")
+
+number = int(input("Digite um número inteiro: "))
+result = int((0.5 + (number * 0.5)) * number)
+print("O resultado de %d! é: %d" %(number, result))
